d84a2abed7f81155013c42cb409713f3706820f7
Jason Sebastian <jasonnsebastian@gmail.com>
2023-09-04 00:20:42 +0800

    Fix deploy issues

--- src/secrets.js
+++ src/secrets.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://jasonsebastian.github.io/";
+export const BASE_URL = "https://jasonsebastian.github.io/spiritual-gifts-survey";
 export const MASTER_KEY = "$2b$10$AxDkq.jguxqiTvxw0E45RujQ33iup0rVDV4b.krWcZcTr2BJiTMpe";
