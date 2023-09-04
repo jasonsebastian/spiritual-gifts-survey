d270b9aa664b97b4ae69d2d9a7aabaf25529ee4e
Jason Sebastian <jasonnsebastian@gmail.com>
2023-09-04 08:47:37 +0800

    Test 3 questions

--- src/questions.js
+++ src/questions.js
@@ -13,314 +13,314 @@ export const questions = [
         id: 3,
         text: "3. I am able to relate the truths of God to specific situations.",
     },
-    {
-        id: 4,
-        text: "4. I have a God-given ability to help others grow in their faith.",
-    },
-    {
-        id: 5,
-        text: "5. I possess a special ability to communicate the truth of salvation.",
-    },
-    {
-        id: 6,
-        text: "6. I have the ability to make critical decisions when necessary.",
-    },
-    {
-        id: 7,
-        text: "7. I am sensitive to the hurts of people.",
-    },
-    {
-        id: 8,
-        text: "8. I experience joy in meeting needs through sharing possessions.",
-    },
-    {
-        id: 9,
-        text: "9. I enjoy studying.",
-    },
-    {
-        id: 10,
-        text: "10. I have delivered God’s message of warning and judgment.",
-    },
-    {
-        id: 11,
-        text: "11. I am able to sense the true motivation of persons and movements.",
-    },
-    {
-        id: 12,
-        text: "12. I have a special ability to trust God in difficult situations.",
-    },
-    {
-        id: 13,
-        text: "13. I have a strong desire to contribute to the establishment of new churches.",
-    },
-    {
-        id: 14,
-        text: "14. I take action to meet physical and practical needs rather than merely talking about",
-    },
-    {
-        id: 15,
-        text: "15. I enjoy entertaining guests in my home.",
-    },
-    {
-        id: 16,
-        text: "16. I can adapt my guidance to fit the maturity of those working with me.",
-    },
-    {
-        id: 17,
-        text: "17. I can delegate and assign meaningful work.",
-    },
-    {
-        id: 18,
-        text: "18. I have an ability and desire to teach.",
-    },
-    {
-        id: 19,
-        text: "19. I am usually able to analyze a situation correctly.",
-    },
-    {
-        id: 20,
-        text: "20. I have a natural tendency to encourage others.",
-    },
-    {
-        id: 21,
-        text: "21. I am willing to take the initiative in helping other Christians grow in their faith.",
-    },
-    {
-        id: 22,
-        text: "22. I have an acute awareness of other people’s emotions, such as loneliness, pain, fear, and anger.",
-    },
-    {
-        id: 23,
-        text: "23. I am a cheerful giver.",
-    },
-    {
-        id: 24,
-        text: "24. I spend time digging into facts.",
-    },
-    {
-        id: 25,
-        text: "25. I feel that I have a message from God to deliver to others.",
-    },
-    {
-        id: 26,
-        text: "26. I can recognize when a person is genuine/honest.",
-    },
-    {
-        id: 27,
-        text: "27. I am a person of vision (a clear mental portrait of a preferable future given by God). I am",
-    },
-    {
-        id: 28,
-        text: "28. I am willing to yield to God’s will rather than question and waver.",
-    },
-    {
-        id: 29,
-        text: "29. I would like to be more active in getting the gospel to people in other countries.",
-    },
-    {
-        id: 30,
-        text: "30. It makes me happy to do things for people in need.",
-    },
-    {
-        id: 31,
-        text: "31. I am successful in getting a group to do its work joyfully.",
-    },
-    {
-        id: 32,
-        text: "32. I am able to make strangers feel at ease.",
-    },
-    {
-        id: 33,
-        text: "33. I have the ability to teach to a variety of different learning styles.",
-    },
-    {
-        id: 34,
-        text: "34. I can identify those who need encouragement.",
-    },
-    {
-        id: 35,
-        text: "35. I have trained Christians to be more obedient disciples of Christ.",
-    },
-    {
-        id: 36,
-        text: "36. I am willing to do whatever it takes to see others come to Christ.",
-    },
-    {
-        id: 37,
-        text: "37. I am drawn to people who are hurting.",
-    },
-    {
-        id: 38,
-        text: "38. I am a generous giver.",
-    },
-    {
-        id: 39,
-        text: "39. I am able to discover new truths in Scripture.",
-    },
-    {
-        id: 40,
-        text: "40. I have spiritual insights from Scripture concerning issues and people that compel me to speak out.",
-    },
-    {
-        id: 41,
-        text: "41. I can sense when a person is acting in accordance with God’s will.",
-    },
-    {
-        id: 42,
-        text: "42. I can trust in God even when things look dark.",
-    },
-    {
-        id: 43,
-        text: "43. I can determine where God wants a group to go and help it get there.",
-    },
-    {
-        id: 44,
-        text: "44. I have a strong desire to take the gospel to places where it has never been heard.",
-    },
-    {
-        id: 45,
-        text: "45. I enjoy reaching out to new people in my church and community.",
-    },
-    {
-        id: 46,
-        text: "46. I am sensitive to the needs of people.",
-    },
-    {
-        id: 47,
-        text: "47. I have been able to make effective and efficient plans for accomplishing the goals of a group.",
-    },
-    {
-        id: 48,
-        text: "48. I often am consulted when fellow Christians are struggling to make difficult decisions.",
-    },
-    {
-        id: 49,
-        text: "49. I think about how I can comfort and encourage others in my congregation.",
-    },
-    {
-        id: 50,
-        text: "50. I am able to give spiritual direction to others.",
-    },
-    {
-        id: 51,
-        text: "51. I am able to present the gospel to lost persons in such a way that they accept the Lord",
-    },
-    {
-        id: 52,
-        text: "52. I possess an unusual capacity to understand the feelings of those in distress.",
-    },
-    {
-        id: 53,
-        text: "53. I have a strong sense of stewardship based on the recognition that God owns all things.",
-    },
-    {
-        id: 54,
-        text: "54. I have delivered to other persons messages that have come directly from God.",
-    },
-    {
-        id: 55,
-        text: "55. I can sense when a person is acting under God’s leadership.",
-    },
-    {
-        id: 56,
-        text: "56. I try to be in God’s will continually and be available for His use.",
-    },
-    {
-        id: 57,
-        text: "57. I feel that I should take the gospel to people who have different beliefs from me.",
-    },
-    {
-        id: 58,
-        text: "58. I have an acute awareness of the physical needs of others.",
-    },
-    {
-        id: 59,
-        text: "59. I am skilled in setting forth positive and precise steps of action.",
-    },
-    {
-        id: 60,
-        text: "60. I like to meet visitors at church and make them feel welcome.",
-    },
-    {
-        id: 61,
-        text: "61. I explain Scripture in such a way that others understand it.",
-    },
-    {
-        id: 62,
-        text: "62. I can usually see spiritual solutions to problems.",
-    },
-    {
-        id: 63,
-        text: "63. I welcome opportunities to help people who need comfort, consolation, encouragement, and counseling.",
-    },
-    {
-        id: 64,
-        text: "64. I feel at ease in sharing Christ with nonbelievers.",
-    },
-    {
-        id: 65,
-        text: "65. I can influence others to perform to their highest God-given potential.",
-    },
-    {
-        id: 66,
-        text: "66. I recognize the signs of stress and distress in others.",
-    },
-    {
-        id: 67,
-        text: "67. I desire to give generously and unpretentiously to worthwhile projects and ministries.",
-    },
-    {
-        id: 68,
-        text: "68. I can organize facts into meaningful relationships.",
-    },
-    {
-        id: 69,
-        text: "69. God gives me messages to deliver to His people.",
-    },
-    {
-        id: 70,
-        text: "70. I am able to sense whether people are being honest when they tell of their religious experiences.",
-    },
-    {
-        id: 71,
-        text: "71. I enjoy presenting the gospel to persons of other cultures and backgrounds.",
-    },
-    {
-        id: 72,
-        text: "72. I enjoy doing little things that help people.",
-    },
-    {
-        id: 73,
-        text: "73. I can give a clear, uncomplicated presentation of the gospel.",
-    },
-    {
-        id: 74,
-        text: "74. I have been able to apply biblical truth to the specific needs of my church.",
-    },
-    {
-        id: 75,
-        text: "75. God has used me to encourage others to live Christlike lives.",
-    },
-    {
-        id: 76,
-        text: "76. I have sensed the need to help other people become more effective in their ministries.",
-    },
-    {
-        id: 77,
-        text: "77. I like to talk about Jesus to those who do not know Him.",
-    },
-    {
-        id: 78,
-        text: "78. I have the ability to make strangers feel comfortable in my home.",
-    },
-    {
-        id: 79,
-        text: "79. I have a wide range of study resources and know how to secure information.",
-    },
-    {
-        id: 80,
-        text: "80. I feel assured that a situation will change for the glory of God even when the situation seem impossible.",
-    }
+    // {
+    //     id: 4,
+    //     text: "4. I have a God-given ability to help others grow in their faith.",
+    // },
+    // {
+    //     id: 5,
+    //     text: "5. I possess a special ability to communicate the truth of salvation.",
+    // },
+    // {
+    //     id: 6,
+    //     text: "6. I have the ability to make critical decisions when necessary.",
+    // },
+    // {
+    //     id: 7,
+    //     text: "7. I am sensitive to the hurts of people.",
+    // },
+    // {
+    //     id: 8,
+    //     text: "8. I experience joy in meeting needs through sharing possessions.",
+    // },
+    // {
+    //     id: 9,
+    //     text: "9. I enjoy studying.",
+    // },
+    // {
+    //     id: 10,
+    //     text: "10. I have delivered God’s message of warning and judgment.",
+    // },
+    // {
+    //     id: 11,
+    //     text: "11. I am able to sense the true motivation of persons and movements.",
+    // },
+    // {
+    //     id: 12,
+    //     text: "12. I have a special ability to trust God in difficult situations.",
+    // },
+    // {
+    //     id: 13,
+    //     text: "13. I have a strong desire to contribute to the establishment of new churches.",
+    // },
+    // {
+    //     id: 14,
+    //     text: "14. I take action to meet physical and practical needs rather than merely talking about",
+    // },
+    // {
+    //     id: 15,
+    //     text: "15. I enjoy entertaining guests in my home.",
+    // },
+    // {
+    //     id: 16,
+    //     text: "16. I can adapt my guidance to fit the maturity of those working with me.",
+    // },
+    // {
+    //     id: 17,
+    //     text: "17. I can delegate and assign meaningful work.",
+    // },
+    // {
+    //     id: 18,
+    //     text: "18. I have an ability and desire to teach.",
+    // },
+    // {
+    //     id: 19,
+    //     text: "19. I am usually able to analyze a situation correctly.",
+    // },
+    // {
+    //     id: 20,
+    //     text: "20. I have a natural tendency to encourage others.",
+    // },
+    // {
+    //     id: 21,
+    //     text: "21. I am willing to take the initiative in helping other Christians grow in their faith.",
+    // },
+    // {
+    //     id: 22,
+    //     text: "22. I have an acute awareness of other people’s emotions, such as loneliness, pain, fear, and anger.",
+    // },
+    // {
+    //     id: 23,
+    //     text: "23. I am a cheerful giver.",
+    // },
+    // {
+    //     id: 24,
+    //     text: "24. I spend time digging into facts.",
+    // },
+    // {
+    //     id: 25,
+    //     text: "25. I feel that I have a message from God to deliver to others.",
+    // },
+    // {
+    //     id: 26,
+    //     text: "26. I can recognize when a person is genuine/honest.",
+    // },
+    // {
+    //     id: 27,
+    //     text: "27. I am a person of vision (a clear mental portrait of a preferable future given by God). I am",
+    // },
+    // {
+    //     id: 28,
+    //     text: "28. I am willing to yield to God’s will rather than question and waver.",
+    // },
+    // {
+    //     id: 29,
+    //     text: "29. I would like to be more active in getting the gospel to people in other countries.",
+    // },
+    // {
+    //     id: 30,
+    //     text: "30. It makes me happy to do things for people in need.",
+    // },
+    // {
+    //     id: 31,
+    //     text: "31. I am successful in getting a group to do its work joyfully.",
+    // },
+    // {
+    //     id: 32,
+    //     text: "32. I am able to make strangers feel at ease.",
+    // },
+    // {
+    //     id: 33,
+    //     text: "33. I have the ability to teach to a variety of different learning styles.",
+    // },
+    // {
+    //     id: 34,
+    //     text: "34. I can identify those who need encouragement.",
+    // },
+    // {
+    //     id: 35,
+    //     text: "35. I have trained Christians to be more obedient disciples of Christ.",
+    // },
+    // {
+    //     id: 36,
+    //     text: "36. I am willing to do whatever it takes to see others come to Christ.",
+    // },
+    // {
+    //     id: 37,
+    //     text: "37. I am drawn to people who are hurting.",
+    // },
+    // {
+    //     id: 38,
+    //     text: "38. I am a generous giver.",
+    // },
+    // {
+    //     id: 39,
+    //     text: "39. I am able to discover new truths in Scripture.",
+    // },
+    // {
+    //     id: 40,
+    //     text: "40. I have spiritual insights from Scripture concerning issues and people that compel me to speak out.",
+    // },
+    // {
+    //     id: 41,
+    //     text: "41. I can sense when a person is acting in accordance with God’s will.",
+    // },
+    // {
+    //     id: 42,
+    //     text: "42. I can trust in God even when things look dark.",
+    // },
+    // {
+    //     id: 43,
+    //     text: "43. I can determine where God wants a group to go and help it get there.",
+    // },
+    // {
+    //     id: 44,
+    //     text: "44. I have a strong desire to take the gospel to places where it has never been heard.",
+    // },
+    // {
+    //     id: 45,
+    //     text: "45. I enjoy reaching out to new people in my church and community.",
+    // },
+    // {
+    //     id: 46,
+    //     text: "46. I am sensitive to the needs of people.",
+    // },
+    // {
+    //     id: 47,
+    //     text: "47. I have been able to make effective and efficient plans for accomplishing the goals of a group.",
+    // },
+    // {
+    //     id: 48,
+    //     text: "48. I often am consulted when fellow Christians are struggling to make difficult decisions.",
+    // },
+    // {
+    //     id: 49,
+    //     text: "49. I think about how I can comfort and encourage others in my congregation.",
+    // },
+    // {
+    //     id: 50,
+    //     text: "50. I am able to give spiritual direction to others.",
+    // },
+    // {
+    //     id: 51,
+    //     text: "51. I am able to present the gospel to lost persons in such a way that they accept the Lord",
+    // },
+    // {
+    //     id: 52,
+    //     text: "52. I possess an unusual capacity to understand the feelings of those in distress.",
+    // },
+    // {
+    //     id: 53,
+    //     text: "53. I have a strong sense of stewardship based on the recognition that God owns all things.",
+    // },
+    // {
+    //     id: 54,
+    //     text: "54. I have delivered to other persons messages that have come directly from God.",
+    // },
+    // {
+    //     id: 55,
+    //     text: "55. I can sense when a person is acting under God’s leadership.",
+    // },
+    // {
+    //     id: 56,
+    //     text: "56. I try to be in God’s will continually and be available for His use.",
+    // },
+    // {
+    //     id: 57,
+    //     text: "57. I feel that I should take the gospel to people who have different beliefs from me.",
+    // },
+    // {
+    //     id: 58,
+    //     text: "58. I have an acute awareness of the physical needs of others.",
+    // },
+    // {
+    //     id: 59,
+    //     text: "59. I am skilled in setting forth positive and precise steps of action.",
+    // },
+    // {
+    //     id: 60,
+    //     text: "60. I like to meet visitors at church and make them feel welcome.",
+    // },
+    // {
+    //     id: 61,
+    //     text: "61. I explain Scripture in such a way that others understand it.",
+    // },
+    // {
+    //     id: 62,
+    //     text: "62. I can usually see spiritual solutions to problems.",
+    // },
+    // {
+    //     id: 63,
+    //     text: "63. I welcome opportunities to help people who need comfort, consolation, encouragement, and counseling.",
+    // },
+    // {
+    //     id: 64,
+    //     text: "64. I feel at ease in sharing Christ with nonbelievers.",
+    // },
+    // {
+    //     id: 65,
+    //     text: "65. I can influence others to perform to their highest God-given potential.",
+    // },
+    // {
+    //     id: 66,
+    //     text: "66. I recognize the signs of stress and distress in others.",
+    // },
+    // {
+    //     id: 67,
+    //     text: "67. I desire to give generously and unpretentiously to worthwhile projects and ministries.",
+    // },
+    // {
+    //     id: 68,
+    //     text: "68. I can organize facts into meaningful relationships.",
+    // },
+    // {
+    //     id: 69,
+    //     text: "69. God gives me messages to deliver to His people.",
+    // },
+    // {
+    //     id: 70,
+    //     text: "70. I am able to sense whether people are being honest when they tell of their religious experiences.",
+    // },
+    // {
+    //     id: 71,
+    //     text: "71. I enjoy presenting the gospel to persons of other cultures and backgrounds.",
+    // },
+    // {
+    //     id: 72,
+    //     text: "72. I enjoy doing little things that help people.",
+    // },
+    // {
+    //     id: 73,
+    //     text: "73. I can give a clear, uncomplicated presentation of the gospel.",
+    // },
+    // {
+    //     id: 74,
+    //     text: "74. I have been able to apply biblical truth to the specific needs of my church.",
+    // },
+    // {
+    //     id: 75,
+    //     text: "75. God has used me to encourage others to live Christlike lives.",
+    // },
+    // {
+    //     id: 76,
+    //     text: "76. I have sensed the need to help other people become more effective in their ministries.",
+    // },
+    // {
+    //     id: 77,
+    //     text: "77. I like to talk about Jesus to those who do not know Him.",
+    // },
+    // {
+    //     id: 78,
+    //     text: "78. I have the ability to make strangers feel comfortable in my home.",
+    // },
+    // {
+    //     id: 79,
+    //     text: "79. I have a wide range of study resources and know how to secure information.",
+    // },
+    // {
+    //     id: 80,
+    //     text: "80. I feel assured that a situation will change for the glory of God even when the situation seem impossible.",
+    // }
 ];
 
 questions.forEach((q) => {
